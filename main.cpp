47934fc336b55ad9ec9cd52213fa33c1e3e3a469
jose.moracubillo <jose.moracubillo@git.ucr.ac.cr>
2024-09-23 01:26:27 -0600

    Updated main

--- main.cpp
+++ main.cpp
@@ -9,57 +9,42 @@ using namespace std;
 int main() {
   Autenticador objectA;
   FAT* objectFAT = objectA.getData();
-  char *filename = (char *)"test.txt";
+  char *filename = (char *)"users.txt";
   char *date = (char *)"12/12/2012";
-  char *proof = (char *)"12/aaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaccccccccccc12/12";
-  bool result4 = objectFAT->create(filename, date);
-  int result = objectFAT->search(filename);
-  bool result2 = objectFAT->open(filename, 7);
-  bool result5 = objectFAT->write(filename, 7, proof);
- 
-
-  Usuario myUser;
-  myUser.addUser("admin", "root", filename, objectFAT);
-  myUser.print(true);
-  
-  char *username = (char *)"admin";
-  char *hash = (char *)"urrw";
-  
-  bool autenticado = objectA.autenticar(username, hash);
- if (autenticado) {
-    std::cout << "usuario verificado!" << std::endl;
-} else { 
-    std::cout << "usuario no verificado!" << std::endl;
-}
-
-   
+  std::cout << "********** (1) Imprimir antes de crear archivos y agregar usuarios (1) **********" << std::endl << std::endl;
   objectFAT->print(true);
-  bool result3 = objectFAT->close(filename, 7);
+  std::cout << std::endl;
+  std::cout << "********** (2) Imprimir después de crear archivos (2) **********" << std::endl << std::endl;
+  objectFAT->create(filename, date);
+  objectFAT->print(true);
+  objectFAT->open(filename, 7);
+  std::cout << std::endl;
+ 
+  Usuario myUser;
+  
+  std::cout << "********** (3) Imprimir después de agregar usuarios (3) **********" << std::endl << std::endl;
+  objectFAT->write(filename, 7, (char*)"Chanchito hx9242\n");
+  objectFAT->append(filename, 7, (char*)"Papita z22342\n");
+  myUser.addUser("admin", "root", filename, objectFAT);
+  objectFAT->print(true);
+  std::cout << std::endl;
+  //myUser.print(true);
 
-  if (result > -1) {
-    std::cout << "funciona search" << std::endl;
-    ;
+  std::cout << "********** (4) Verificación (4) **********" << std::endl << std::endl;
+  bool autenticado = objectA.autenticar((char *)"admin", (char *)"urrw");
+  if (autenticado) {
+      std::cout << "admin" << ": usuario verificado!" << std::endl;
+  } else { 
+      std::cout << "admin" << "usuario no verificado!" << std::endl;
+  }
+  autenticado = objectA.autenticar((char *)"Papita", (char *)"mayonesa");
+  if (autenticado) {
+      std::cout << "Papita" << ": usuario verificado!" << std::endl;
+  } else { 
+      std::cout << "Papita" << ": usuario no verificado!" << std::endl;
   }
 
-  if (result2) {
-    std::cout << "funciona open" << std::endl;
-    ;
-  }
-
-  if (result3) {
-    std::cout << "funciona close" << std::endl;
-    ;
-  }
-
-  if (result4) {
-    std::cout << "funciona create" << std::endl;
-    ;
-  }
-
-  if (result5) {
-    std::cout << "funciona write" << std::endl;
-    ;
-  }
+  objectFAT->close(filename, 7);
 
   return 0;
 }
