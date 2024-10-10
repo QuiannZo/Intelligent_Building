b530af6f254d55a56c06175c3b49332e57d773ef
Ximena De Los Angele Marin Sanchez <c14448@ECCI102L393214.ecci.ucr.ac.cr>
2024-10-10 11:20:48 -0600

    Codigo Arduino
    Funciona para fotoresistor e infrarojo

---
+++ Intento.ino
@@ -0,0 +1,18 @@
+// Photoresistor Code
+
+int ldr = 0;
+int ldr_value = 0;
+
+
+void setup()
+{
+  Serial.begin (9600);
+}
+
+void loop()
+{
+  ldr_value = analogRead (ldr);
+  Serial.println (ldr_value);
+  
+  delay(1000); // Wait for 1000 millisecond(s)
+}
