fbd4acf3d0ec5cd8594b360640755b5d2b50da51
jose.moracubillo <jose.moracubillo@git.ucr.ac.cr>
2024-10-12 23:27:30 -0600

    Added the class corresponding to the log

---
+++ Log.hpp
@@ -0,0 +1,30 @@
+#ifndef LOG_HPP
+#define LOG_HPP
+
+#include "string"
+#include "FAT.hpp"
+
+class Log {
+private:
+    FAT* fileSystem;
+    // Name of the file that contains the log
+    std::string logFilename;
+    // This variable is used to indicate how often should be saved the file system
+    int saveFrequency;
+    int saveCounter = 0;
+    int defaultProcessId;
+    std::string getCurrentDateTime();
+
+
+public:
+    Log(std::string logFilename, int defaultProcessId);
+    ~Log();
+    bool appendToLog(std::string text);
+    bool appendToLogTimeHour(std::string text);
+    std::string returnLog(std::string username);
+    int getProcessId();
+};
+
+
+
+#endif
