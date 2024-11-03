94355739fa9dabb120c19c5e22d1d363fb3deb98
jose.moracubillo <jose.moracubillo@git.ucr.ac.cr>
2024-11-03 01:30:01 -0600

    Added handler for control + c signal

--- etapa3PI/main_intermediary.cpp
+++ etapa3PI/main_intermediary.cpp
@@ -1,12 +1,28 @@
+#include <csignal>
+#include <iostream> 
 #include "Intermediary.hpp"
 #include "networkConfig.hpp"
 
+// Manejo de control + c: basado en https://www.tutorialspoint.com/how-do-i-catch-a-ctrlplusc-event-in-cplusplus
+// Puntero global
+Intermediary* node = nullptr;
+
+void signalControlCHandler(int signum) {
+  if (node) {
+    delete node;
+  }
+  std::exit(signum); 
+}
+
 int main() {
-  Intermediary node = Intermediary("log_intermediary.txt", 1, kIntermediaryPort);
-  if(node.initServer()) {
-    node.run();
+  Intermediary* node = new Intermediary("log_intermediary.txt", 1, kIntermediaryPort);
+  signal(SIGINT, signalControlCHandler); 
+  if(node->initServer()) {
+    node->run();
+    delete node;
     return 0;
   } else {
+    delete node;
     return 1;
   }
 }
--- etapa3PI/main_userNode.cpp
+++ etapa3PI/main_userNode.cpp
@@ -1,12 +1,28 @@
+#include <csignal> 
+#include <iostream>
 #include "UserNode.cpp"
 #include "networkConfig.hpp"
 
+// Manejo de control + c: basado en https://www.tutorialspoint.com/how-do-i-catch-a-ctrlplusc-event-in-cplusplus
+// Puntero global
+UserNode* node = nullptr;
+
+void signalControlCHandler(int signum) {
+  if (node) {
+    delete node;
+  }
+  std::exit(signum); 
+}
+
 int main() {
-  UserNode node = UserNode("log_UserNode.txt", 1, "usersData.txt", kUserHandlerPort);
-  if(node.initServer()) {
-    node.run();
+  UserNode* node = new UserNode("log_UserNode.txt", 1, "usersData.txt", kUserHandlerPort);
+  signal(SIGINT, signalControlCHandler); 
+  if(node->initServer()) {
+    node->run();
+    delete node;
     return 0;
   } else {
+    delete node;
     return 1;
   }
 }
